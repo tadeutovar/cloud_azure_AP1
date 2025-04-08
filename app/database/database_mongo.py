4fd6c41585953837c70c76d4728dea41e50cbcf9
Tadeu Tovar <113369978+tadeutovar@users.noreply.github.com>
2025-04-08 16:22:18 -0300

    Removendo chave do código

--- app/database/database_mongo.py
+++ app/database/database_mongo.py
@@ -1,6 +1,10 @@
+from dotenv import load_dotenv
+import os
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb://cosmosteste1:VAkllNZUbPVfE2fB2BsPWLTDoS6UkcOfJIUjkPWaJg56rrZPZQjUnR5IBIDvzk6pKQFHOx4AKl3CACDbSIwWdA==@cosmosteste1.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@cosmosteste1@"
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
 
 client = MongoClient(MONGO_URI)
 
